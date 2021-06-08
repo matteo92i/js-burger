b3c29a1e441d04d92d462c750d14de72f29da507
Matteo92i <matteo.iovane@gmail.com>
2021-06-08 12:55:42 +0200

    script

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var calcolaPrezzo = document.getElementById('calcolaPrezzo')
+
+var sommaBurger = document.getElementById('sommaFinale');
+
+calcolaPrezzo.addEventListener( 'click', function() {
+    var checkSomma = document.getElementsByClassName('check-somma');
+
+    for(var i =0; i < checkSomma.length; i++){
+        if(checkSomma[i].checked){
+            
+        }
+    }
+})
